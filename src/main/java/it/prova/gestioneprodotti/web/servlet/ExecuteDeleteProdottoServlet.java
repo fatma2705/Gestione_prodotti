ea0a314783d49c2bd472e58a9cafe970a19fc183
fatma2705 <arafatma2003@gmail.com>
2024-05-06 16:17:52 +0200

    creazione servlet execute delete

---
+++ src/main/java/it/prova/gestioneprodotti/web/servlet/ExecuteDeleteProdottoServlet.java
@@ -0,0 +1,43 @@
+package it.prova.gestioneprodotti.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+import it.prova.gestioneprodotti.service.MyServiceFactory;
+
+@WebServlet("/ExecuteDeleteProdottoServlet")
+public class ExecuteDeleteProdottoServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String parametroIdProdottoToRemove = request.getParameter("idProdottoToRemove");
+		Long idProdottoToRemove = Long.parseLong(parametroIdProdottoToRemove);
+		if (!NumberUtils.isCreatable(parametroIdProdottoToRemove)) {
+			request.setAttribute("errorMessage", "Attenzione si è verificato un errore");
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			return;
+		}
+		try {
+			MyServiceFactory.getProdottoServiceInstance().delete(idProdottoToRemove);
+			request.setAttribute("listaProdottiAttribute", MyServiceFactory.getProdottoServiceInstance().list());
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("errorMessage", "Attenzione si è verificato un errore");
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			return;
+		}
+
+		request.getRequestDispatcher("/prodotto/results.jsp").forward(request, response);
+	}
+
+}
