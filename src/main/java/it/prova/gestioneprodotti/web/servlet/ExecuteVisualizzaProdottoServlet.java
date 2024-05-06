b294468915515cceff2b766902328bf812e565ff
fatma2705 <arafatma2003@gmail.com>
2024-05-06 16:31:37 +0200

    creazione servlet visualizza prodotto

---
+++ src/main/java/it/prova/gestioneprodotti/web/servlet/ExecuteVisualizzaProdottoServlet.java
@@ -0,0 +1,44 @@
+package it.prova.gestioneprodotti.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+import it.prova.gestioneprodotti.service.MyServiceFactory;
+
+@WebServlet("/ExecuteVisualizzaProdottoServlet")
+public class ExecuteVisualizzaProdottoServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String idProdottoParam = request.getParameter("idProdotto");
+
+		if (!NumberUtils.isCreatable(idProdottoParam)) {
+			// qui ci andrebbe un messaggio nei file di log costruito ad hoc se fosse attivo
+			request.setAttribute("errorMessage", "Attenzione si è verificato un errore.");
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			return;
+		}
+		try {
+			request.setAttribute("visualizza_prodotto_attr",
+					MyServiceFactory.getProdottoServiceInstance().findOne(Long.parseLong(idProdottoParam)));
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("errorMessage", "Attenzione si è verificato un errore.");
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			return;
+		}
+
+		request.getRequestDispatcher("/prodotto/show.jsp").forward(request, response);
+
+	}
+
+}
