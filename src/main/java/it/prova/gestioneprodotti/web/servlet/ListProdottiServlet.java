1b545030ef2b4ffa8e26f134080cf096b7588ce8
fatma2705 <arafatma2003@gmail.com>
2024-05-06 12:03:52 +0200

    creazione servlet list prodotti

---
+++ src/main/java/it/prova/gestioneprodotti/web/servlet/ListProdottiServlet.java
@@ -0,0 +1,31 @@
+package it.prova.gestioneprodotti.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.prova.gestioneprodotti.service.MyServiceFactory;
+
+@WebServlet("/ListProdottiServlet")
+public class ListProdottiServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		try {
+			request.setAttribute("listArticoliAttribute", MyServiceFactory.getProdottoServiceInstance().list());
+		} catch (Exception e) {
+			//qui ci andrebbe un messaggio nei file di log costruito ad hoc se fosse attivo
+			e.printStackTrace();
+			request.setAttribute("errorMessage", "Attenzione si è verificato un errore.");
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+			return;
+		}
+		request.getRequestDispatcher("/prodotto/results.jsp").forward(request, response);
+	}
+	
+
+}
